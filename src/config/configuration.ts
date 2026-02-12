b03735446abb512a035d5efe104b0b9de411f2e5
John Sanchez <john.sanchez@intigrate.net>
2026-02-12 11:08:23 -0500

    feat: configuration service
    [x] Configuration Service: Read environment variables for database connection and server specifications
    [x] Validation: validation using joi for environment variables

---
+++ src/config/configuration.ts
@@ -0,0 +1,19 @@
+export default () => ({
+  port: Number.parseInt(process.env.PORT as string, 10) || 3000,
+  database: {
+    url: process.env.DB_URL ?? 
+    `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?schema=public`,
+    host: process.env.DB_HOST,
+    port: Number.parseInt(process.env.DB_PORT as string, 10) || 5432,
+    user: process.env.DB_USERNAME,
+    pass: process.env.DB_PASSWORD,
+    name: process.env.DB_NAME,
+  },
+  security: {
+    corsOrigin: process.env.CORS_ORIGIN,
+    rateLimit: {
+      ttl: Number.parseInt(process.env.RATE_LIMIT_TTL as string, 10),
+      limit: Number.parseInt(process.env.RATE_LIMIT_LIMIT as string, 10),
+    }
+  },
+});
---
+++ src/config/validation.ts
@@ -0,0 +1,23 @@
+import * as Joi from 'joi';
+
+export const validationSchema = Joi.object({
+  // Base
+  NODE_ENV: Joi.string().valid('development', 'production', 'test').default('development'),
+  PORT: Joi.number().default(3000),
+  
+  // Database
+  DB_URL: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(5432),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().allow(''),
+  DB_NAME: Joi.string().required(),
+
+  // Security & Limits
+  CORS_ORIGIN: Joi.string().required(), // e.g., http://localhost:3000
+  RATE_LIMIT_TTL: Joi.number().default(60),
+  RATE_LIMIT_LIMIT: Joi.number().default(10),
+  
+  // App Specific
+  GLOBAL_PREFIX: Joi.string().default('api/v1'),
+});
